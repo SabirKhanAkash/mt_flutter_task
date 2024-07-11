68e5c5ac8e034a423c437b6dd38c43e662328d0c
Sabir Khan Akash <sabir.akash.cs@gmail.com>
2024-07-11 18:10:42 +0600

    added repositories

---
+++ lib/data/repositories/album_repository.dart
@@ -0,0 +1,103 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:mt_flutter_task/data/models/api_response_model.dart';
+import 'package:mt_flutter_task/data/providers/album_data.dart';
+import 'package:mt_flutter_task/utils/console.dart';
+import 'package:mt_flutter_task/utils/toast.dart';
+import 'package:provider/provider.dart';
+
+class AlbumRepository {
+  final String baseUrl;
+  final http.Client httpClient;
+
+  AlbumRepository({required this.baseUrl, required this.httpClient});
+
+  Future<ApiResponse> getAlbumList(BuildContext context, int? pageNo) async {
+    var _data = Provider.of<AlbumDataProvider>(context, listen: false);
+    Future.delayed(Duration.zero, () async {
+      _data.startLoading();
+    });
+    try {
+      final response = await httpClient.get(
+        Uri.parse('$baseUrl/albums')
+            .replace(queryParameters: {'_page': '$pageNo'}),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      );
+
+      Console.log("Endpoint: $baseUrl/albums?_page=$pageNo");
+
+      if (response.statusCode == 200) {
+        _data.dismissLoading();
+        Console.log("A1. Response from API: ${response.body}");
+        return ApiResponse.fromJson(jsonDecode(response.body));
+      } else {
+        _data.dismissLoading();
+        Console.log("Status Code ${response.statusCode}: ${response.body}");
+        switch (response.statusCode) {
+          case 401:
+            throw Exception('401 Unauthorized');
+          case 400:
+            throw Exception('400 Bad Request');
+          case 404:
+            throw Exception('404 Not Found');
+          default:
+            throw Exception(
+                'Album list showing Failed (Status Code: ${response.statusCode})');
+        }
+      }
+    } on Exception catch (error) {
+      _data.dismissLoading();
+      // _data.setNoInternetScreen();
+      showCustomToast(context, "${error.toString()}", "negative");
+      throw Exception('An Error Occurred: ${error.toString()}');
+    }
+  }
+
+  Future<ApiResponse> getAlbumSearchList(
+      BuildContext context, String? query) async {
+    var _data = Provider.of<AlbumDataProvider>(context, listen: false);
+    Future.delayed(Duration.zero, () async {
+      _data.startLoading();
+    });
+    try {
+      final response = await httpClient.get(
+        Uri.parse('$baseUrl/albums')
+            .replace(queryParameters: {'title': '$query'}),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      );
+
+      Console.log("Endpoint: $baseUrl/albums?title=$query");
+
+      if (response.statusCode == 200) {
+        _data.dismissLoading();
+        Console.log("A1. Response from API: ${response.body}");
+        return ApiResponse.fromJson(jsonDecode(response.body));
+      } else {
+        _data.dismissLoading();
+        Console.log("Status Code ${response.statusCode}: ${response.body}");
+        switch (response.statusCode) {
+          case 401:
+            throw Exception('401 Unauthorized');
+          case 400:
+            throw Exception('400 Bad Request');
+          case 404:
+            throw Exception('404 Not Found');
+          default:
+            throw Exception(
+                'Album list showing Failed (Status Code: ${response.statusCode})');
+        }
+      }
+    } on Exception catch (error) {
+      _data.dismissLoading();
+      // _data.setNoInternetScreen();
+      showCustomToast(context, "${error.toString()}", "negative");
+      throw Exception('An Error Occurred: ${error.toString()}');
+    }
+  }
+}
---
+++ lib/data/repositories/photo_repository.dart
@@ -0,0 +1,104 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:mt_flutter_task/data/models/api_response_model.dart';
+import 'package:mt_flutter_task/data/providers/photo_data.dart';
+import 'package:mt_flutter_task/utils/console.dart';
+import 'package:mt_flutter_task/utils/toast.dart';
+import 'package:provider/provider.dart';
+
+class PhotoRepository {
+  final String baseUrl;
+  final http.Client httpClient;
+
+  PhotoRepository({required this.baseUrl, required this.httpClient});
+
+  Future<ApiResponse> getPhotoList(
+      BuildContext context, int? pageNo, int? albumId) async {
+    var _data = Provider.of<PhotoDataProvider>(context, listen: false);
+    Future.delayed(Duration.zero, () async {
+      _data.startLoading();
+    });
+    try {
+      final response = await httpClient.get(
+        Uri.parse('$baseUrl/albums/$albumId/photos')
+            .replace(queryParameters: {'_page': '$pageNo'}),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      );
+
+      Console.log("Endpoint: $baseUrl/albums/$albumId/photos?_page=$pageNo");
+
+      if (response.statusCode == 200) {
+        _data.dismissLoading();
+        Console.log("P1. Response from API: ${response.body}");
+        return ApiResponse.fromJson(jsonDecode(response.body));
+      } else {
+        _data.dismissLoading();
+        Console.log("Status Code ${response.statusCode}: ${response.body}");
+        switch (response.statusCode) {
+          case 401:
+            throw Exception('401 Unauthorized');
+          case 400:
+            throw Exception('400 Bad Request');
+          case 404:
+            throw Exception('404 Not Found');
+          default:
+            throw Exception(
+                'Photo list showing Failed (Status Code: ${response.statusCode})');
+        }
+      }
+    } on Exception catch (error) {
+      _data.dismissLoading();
+      // _data.setNoInternetScreen();
+      showCustomToast(context, "${error.toString()}", "negative");
+      throw Exception('An Error Occurred: ${error.toString()}');
+    }
+  }
+
+  Future<ApiResponse> getPhotoSearchList(
+      BuildContext context, String? query, int? albumId) async {
+    var _data = Provider.of<PhotoDataProvider>(context, listen: false);
+    Future.delayed(Duration.zero, () async {
+      _data.startLoading();
+    });
+    try {
+      final response = await httpClient.get(
+        Uri.parse('$baseUrl/albums/$albumId/photos')
+            .replace(queryParameters: {'title': '$query'}),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      );
+
+      Console.log("Endpoint: $baseUrl/albums/$albumId/photos?title=$query");
+
+      if (response.statusCode == 200) {
+        _data.dismissLoading();
+        Console.log("P2. Response from API: ${response.body}");
+        return ApiResponse.fromJson(jsonDecode(response.body));
+      } else {
+        _data.dismissLoading();
+        Console.log("Status Code ${response.statusCode}: ${response.body}");
+        switch (response.statusCode) {
+          case 401:
+            throw Exception('401 Unauthorized');
+          case 400:
+            throw Exception('400 Bad Request');
+          case 404:
+            throw Exception('404 Not Found');
+          default:
+            throw Exception(
+                'Photo list showing Failed (Status Code: ${response.statusCode})');
+        }
+      }
+    } on Exception catch (error) {
+      _data.dismissLoading();
+      // _data.setNoInternetScreen();
+      showCustomToast(context, "${error.toString()}", "negative");
+      throw Exception('An Error Occurred: ${error.toString()}');
+    }
+  }
+}
